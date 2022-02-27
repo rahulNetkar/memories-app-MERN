8a55f43da5f52bc6cca756334780ebf37427ad37
Rahul Netkar <rsn9.rn@gmail.com>
2022-02-28 02:17:22 +0530

    client folder setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from './Components/Images/memories.png';
+import Form from './Components/Form/Form';
+import Posts from './Components/Posts/Posts';
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <Container maxWidth='lg' >
+      <AppBar className={classes.appBar} position='static' color='inherit' >
+        <Typography className={classes.heading} variant='h2' align='center' > Memories </Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
+      </AppBar>
+
+      <Grow in >
+        <Container>
+          <Grid container justifyContent='space-between' alignItems='stretch' spacing={3} >
+            <Grid item xs={12} sm={7} >
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4} >
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
