46f5cb97b7f6f59c22909466936011d84e8b1f9a
Rahul Netkar <rsn9.rn@gmail.com>
2022-03-02 00:02:42 +0530

    single post component added

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -27,3 +27,23 @@ export const createPost = (post) => async (dispatch) => {
         console.log(error.message)
     }
 }
+
+export const likePost = (post) => async (dispatch) => {
+    try {
+
+
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export const deletePost = (post) => async (dispatch) => {
+    try {
+
+
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
